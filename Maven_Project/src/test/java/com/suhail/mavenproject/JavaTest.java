4592968e4c1aa109bd9ca62fbe6813cd569b93a6
sarwat-suhail <suhqtp@gmail.com>
2019-01-23 16:44:50 +0530

    javatest modified

--- Maven_Project/src/test/java/com/suhail/mavenproject/JavaTest.java
+++ Maven_Project/src/test/java/com/suhail/mavenproject/JavaTest.java
@@ -1,6 +1,7 @@
 package com.suhail.mavenproject;
 
 
+import org.testng.annotations.Optional;
 import org.testng.annotations.Parameters;
 import org.testng.annotations.Test;
 
@@ -9,7 +10,7 @@ public class JavaTest {
 
 	@Parameters("CollectionKey")
 	@Test()
-	public void Collection_Api(String CollectionKey) {
+	public void Collection_Api(@Optional("Reflection") String CollectionKey) {
 		
 		System.out.println("Collection_Api with "+CollectionKey);
 		
