74cfe21e4ccac56c7125d0d008ee854fb27879fe
sarwat-suhail <suhqtp@gmail.com>
2019-01-23 14:18:58 +0530

    added new java class

---
+++ Maven_Project/src/test/java/com/suhail/mavenproject/JavaTest.java
@@ -0,0 +1,21 @@
+package com.suhail.mavenproject;
+
+import org.testng.annotations.Optional;
+import org.testng.annotations.Parameters;
+import org.testng.annotations.Test;
+
+public class JavaTest {
+	//private static final String[] "Collectionkey" = null;
+
+	@Parameters("CollectionKey")
+	@Test()
+	public void Collection_Api(String keyname) {
+		
+		System.out.println("Collection_Api with "+keyname);
+		
+	}
+	
+	
+	
+	
+}
